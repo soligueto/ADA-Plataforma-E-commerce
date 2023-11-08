4a38a8d973556af77821b41f334f69636c0fb4bf
Elton Soligueto <48188992+soligueto@users.noreply.github.com>
2023-11-08 20:59:14 -0300

    Add files via upload

---
+++ orquestrador/src/main/java/com/br/ada/ecommerce/dtos/PagamentoRequestDto.java
@@ -0,0 +1,11 @@
+package com.br.ada.ecommerce.dtos;
+
+import com.br.ada.ecommerce.dominio.DadosPagamento;
+
+public class PagamentoRequestDto {
+
+    private String vendaId;
+    private String valorPagamento;
+    private DadosPagamentoRequest dadosPagamento;
+
+}
---
+++ orquestrador/src/test/java/com/br/ada/ecommerce/orquestrador/OrquestradorTeste.java
@@ -0,0 +1,13 @@
+package com.br.ada.ecommerce.orquestrador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrquestradorTeste {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
