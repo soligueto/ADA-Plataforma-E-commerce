12fb8b448f3da43e3fe28b531c2420adf9cfd4eb
Elton Soligueto <48188992+soligueto@users.noreply.github.com>
2023-11-08 21:00:00 -0300

    Delete orquestrador directory

--- orquestrador/src/main/java/com/br/ada/ecommerce/dtos/RespostaPagamentoDto.java
+++
@@ -1,9 +0,0 @@
-package com.br.ada.ecommerce.dtos;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-public class RespostaPagamentoDto {
-    @JsonProperty("status_pagamento")
-    private String statusPagamento;
-
-}
